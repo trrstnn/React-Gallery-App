dca7552fe6839d4017d8263d54cd0e822d26c3d4
trrstnn <tristan.declarin@gmail.com>
2019-09-24 00:23:39 -0700

    Updates

---
+++ precache-manifest.fb1918013f462de647d6d1c322ec7a6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94b3c9af4444205a75afe2617522dd9",
+    "url": "/React-Gallery-App/index.html"
+  },
+  {
+    "revision": "939feaa1dffeff8541cb",
+    "url": "/React-Gallery-App/static/css/main.ed434285.chunk.css"
+  },
+  {
+    "revision": "2f75cf35686d6c7a5853",
+    "url": "/React-Gallery-App/static/js/2.338640b8.chunk.js"
+  },
+  {
+    "revision": "939feaa1dffeff8541cb",
+    "url": "/React-Gallery-App/static/js/main.68952007.chunk.js"
+  },
+  {
+    "revision": "0e6417328baa6ca7566c",
+    "url": "/React-Gallery-App/static/js/runtime-main.850b3b3d.js"
+  },
+  {
+    "revision": "5c98c049a661ef0411142ca8c10ed71b",
+    "url": "/React-Gallery-App/static/media/nav-bg.5c98c049.jpg"
+  }
+]);
